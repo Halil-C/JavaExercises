a7e69a392252eb0c384354b522ccae9d39d05e51
Hcicek <56ibrahim@gmail.com>
2020-08-04 15:54:02 -0500

    Commit

--- eclipse-workspace/Chapter3/Chapter3/Chapter3/Exercise03_23.java
+++ eclipse-workspace/Chapter3/Chapter3/Chapter3/Exercise03_23.java
@@ -14,7 +14,6 @@ public class Exercise03_23 {
 		boolean withinRectangle = (Math.pow(Math.pow(x, 2), 0.5) <= 10 / 2 ) &&
 										  (Math.pow(Math.pow(y, 2), 0.5) <= 5.0 / 2);
 
-		// Display results
 		System.out.println("Point (" + x + ", " + y + ") is " +
 			((withinRectangle) ? "in " : "not in ") + "the rectangle");
 	}
