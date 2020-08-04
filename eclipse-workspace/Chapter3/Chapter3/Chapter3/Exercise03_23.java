3a7660f80ebb01bb80ff3cc5ef7b9d55f5471442
Hcicek <56ibrahim@gmail.com>
2020-08-04 15:48:08 -0500

    Commit

--- eclipse-workspace/Chapter3/Chapter3/Chapter3/Exercise03_23.java
+++ eclipse-workspace/Chapter3/Chapter3/Chapter3/Exercise03_23.java
@@ -1,5 +1,21 @@
 package Chapter3;
 
+import java.util.Scanner;
+
 public class Exercise03_23 {
 
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a point with two coordinates: ");
+		double x = input.nextDouble();
+		double y = input.nextDouble();
+
+		boolean withinRectangle = (Math.pow(Math.pow(x, 2), 0.5) <= 10 / 2 ) &&
+										  (Math.pow(Math.pow(y, 2), 0.5) <= 5.0 / 2);
+
+		// Display results
+		System.out.println("Point (" + x + ", " + y + ") is " +
+			((withinRectangle) ? "in " : "not in ") + "the rectangle");
+	}
 }
