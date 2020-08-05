d311ac646187c76fc579ed1df8903711962c3a70
Hcicek <56ibrahim@gmail.com>
2020-08-04 23:59:47 -0500

    Commit

---
+++ eclipse-workspace/Chapter3/Chapter3/Chapter3/Exercise03_30.java
@@ -0,0 +1,35 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class Exercise03_30 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); 	// Create a Scanner
+
+		System.out.print("Enter the time zone offset to GMT: ");
+		int offset = input.nextInt();
+
+		long totalMilliseconds = System.currentTimeMillis();
+
+		long totalSeconds = totalMilliseconds / 1000;
+
+		long currentSecond = totalSeconds % 60;
+
+		long totalMinutes = totalSeconds / 60;
+
+		long currentMinute = totalMinutes % 60;
+
+		long totalHours = totalMinutes / 60;
+
+		long currentHour = totalHours % 24;
+		currentHour = currentHour + offset;
+
+		System.out.println(
+			"Current time is " + ((currentHour > 12) ? currentHour - 12 : 
+			currentHour) + ":" + currentMinute + ":" + currentSecond +
+			((currentHour > 12) ? " PM" : " AM"));
+	}
+	
+	
+}
