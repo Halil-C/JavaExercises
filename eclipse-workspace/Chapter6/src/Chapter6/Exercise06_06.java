bdc42ee488092606e31af13914b159f393e04f8f
Hcicek <56ibrahim@gmail.com>
2020-08-18 22:42:05 -0500

    Commit

---
+++ eclipse-workspace/Chapter6/src/Chapter6/Exercise06_06.java
@@ -0,0 +1,32 @@
+package Chapter6;
+
+import java.util.Scanner;
+
+public class Exercise06_06 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num = input.nextInt();
+		displayPattern(num);
+	}
+
+	public static void displayPattern(int n) {
+
+		for (int row = 1; row <= n; row++) {
+
+			// empty strings to print == n - row
+			for (int formatter = 1; formatter <= n - row; formatter++) {
+				System.out.printf("%4s", "");
+			}
+
+			// numbers to print == row number
+			for (int column = row; column >= 1; column--) {
+				System.out.printf("%4d", column);
+			}
+			System.out.println();
+
+		}
+	}
+}
