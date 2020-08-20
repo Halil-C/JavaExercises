2b93a77c7fb305aab16aaf3a0ca830914e934993
Hcicek <56ibrahim@gmail.com>
2020-08-20 11:35:13 -0500

    Commit

---
+++ eclipse-workspace/Chapter6/src/Chapter6/Exercise06_12.java
@@ -0,0 +1,23 @@
+package Chapter6;
+
+public class Exercise06_12 {
+
+	public static void main(String[] args) {
+
+		printChars('1', 'Z', 10);
+
+	}
+
+	public static void printChars(char ch1, char ch2, int numberPerLine) {
+
+		for (int count = 1; ch1 <= ch2; count++, ch1++) {
+
+			System.out.print(ch1 + " ");
+			if (count % numberPerLine == 0)
+				System.out.println("");
+
+		}
+
+	}
+
+}
