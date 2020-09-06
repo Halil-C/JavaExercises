3e7e2872abd54820562327854fdccdfc1b5bddff
Hcicek <56ibrahim@gmail.com>
2020-09-06 13:13:51 -0500

    Commit

---
+++ eclipse-workspace/Chapter7/src/Chapter7/Exercise07_07.java
@@ -0,0 +1,45 @@
+package Chapter7;
+
+public class Exercise07_07 {
+
+	static final int SIZE = 100;
+    static final int RANGE = 10;
+    public static void main(String[] args) {
+
+        int[] randomNumbers = new int[SIZE];
+        int[] occurrence = new int[RANGE];
+
+        for (int i = 0; i < randomNumbers.length; i++) {
+            int num = (int)(Math.random() * RANGE);
+            randomNumbers[i] = num;
+            occurrence[num]++;
+        }
+
+        for (int i = 0; i < randomNumbers.length; i++) {
+
+            System.out.print(randomNumbers[i] + " ");
+            if ((i + 1) % 10 == 0) System.out.println("");
+        }
+
+        System.out.println("---------------------------");
+        for (int i = 0; i < occurrence.length; i++) {
+            System.out.println("Occurrences for "+ i + " = " + occurrence[i]);
+
+
+        }
+
+
+
+
+    }
+
+    public static void printArray(int[] array, int numberPerLine) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            System.out.printf("%4d", array[i]);
+            if ((i + 1) % numberPerLine == 0) System.out.println("");
+        }
+    }
+	
+}
