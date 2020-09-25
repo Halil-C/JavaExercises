97f57c6c20ad5af9214e6fa3340fdf17a675ef16
Hcicek <56ibrahim@gmail.com>
2020-09-25 10:04:50 -0500

    Commit

---
+++ eclipse-workspace/Chapter7/src/Chapter7/Exercise07_31.java
@@ -0,0 +1,5 @@
+package Chapter7;
+
+public class Exercise07_31 {
+
+}
---
+++ eclipse-workspace/Chapter7/src/Chapter7/Exercise07_33.java
@@ -0,0 +1,21 @@
+package Chapter7;
+
+import java.util.Scanner;
+
+public class Exercise07_33 {
+
+	 public static void main(String[] args) {
+
+	        String[] chineseZodiac = new String[] {
+	                "monkey","rooster","dog","pig","rat","ox","tiger","rabbit","dragon","snake","horse","sheep"
+	        };
+	        Scanner input = new Scanner(System.in);
+
+
+	        System.out.print("Enter a year: ");
+	        int year = input.nextInt();
+
+	        System.out.println(chineseZodiac[year % 12]);
+	    }
+	
+}
