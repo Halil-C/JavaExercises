4b63c9a7f9a591e805a8a3272ab23fbec6ed1014
Hcicek <56ibrahim@gmail.com>
2020-10-11 09:31:13 -0500

    Commit

---
+++ eclipse-workspace/Chapter9/src/Chapter9/Exercise09_04.java
@@ -0,0 +1,20 @@
+package Chapter9;
+
+import java.util.Random;
+
+public class Exercise09_04 {
+	 public static void main(String[] args) {
+
+	        Random random = new Random(1000);
+
+	        for (int i = 0; i < 50; i++) {
+
+	            System.out.printf("%3d ", random.nextInt(101)); // displays 1-99
+	            if ((i + 1) % 10 == 0) {
+	                System.out.println();
+	            }
+	        }
+
+
+	    }
+}
